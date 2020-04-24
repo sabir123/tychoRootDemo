d23491004a31a482945de27fd2994edd56870c20
sabir123 <sabirleads@gmail.com>
2020-04-24 12:23:19 +0300

    Tycho Demo Root folder

---
+++ bundles/com.sab.tycho.rcp.hello.app/src/com/sab/tycho/rcp/hello/app/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,22 @@
+package com.sab.tycho.rcp.hello.app;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "com.sab.tycho.rcp.hello.app.perspective";
+
+	@Override
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+	
+	@Override
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
